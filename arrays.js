a692b16ff4de77ba31e29ab5ccae4cc92bcb1abe
Kas Boyd <97766060+kas-boyd@users.noreply.github.com>
2022-06-30 14:30:47 -0700

    splice + more array methods
    includes using for loop to loop through nested arrays

--- arrays.js
+++ arrays.js
@@ -75,3 +75,79 @@ console.log(names.includes("David")) // returns false
 console.log(names) // returns [ 'Bob', 'John', 'Thomas' ]
 names.reverse(); //reverse method reverses the order of the array, IT DOES NOT PRESERVE THE ORIGINAL
 console.log(names) // returns [ 'Thomas', 'John', 'Bob' ]
+
+console.log(names) // returns [ 'Thomas', 'John', 'Bob' ] (variable defined line 7)
+let singleTermPresidents = names.splice(1,1); // splice mutatest he original array; 
+// it's used to add or remove emlements from the middle of an array (instead of adding or removing from either end)
+// its first argument is an index; the second is a count of elements to delete from the element - starting at the provided index
+// it returns a new array with all of the removed elements.
+console.log(names) // returns [ 'Thomas', 'Bob' ]
+
+        //another example of splice
+        let beatles = ["Paul", "George", "John", "Ringo"]
+        console.log(beatles) // returns [ 'Paul', 'George', 'John', 'Ringo' ]
+        let newGroup = beatles.splice(1,2, "Pete")
+        console.log(beatles) //returns [ 'Paul', 'Ringo' ]
+        console.log(newGroup) //returns [ 'George', 'John' ]
+            //you can also insert values in an array using .splice
+                //let newGroup = beatles.splice(1,2,"Pete") --> serves as a replace 
+                    //returns [ 'Paul', 'Pete, 'Ringo' ] 
+                //spice hack: let newGroup = beatles.splice(1,0, "Pete") 
+                    // returns: nothing in the new array, and adds "Pete" to the original beatles array [ 'Paul', 'Pete', 'George', 'John', 'Ringo' ]
+
+// join method
+let sentence = "The Beatles were " + beatles.join(", ")
+console.log(sentence) // returns "The Beatles were Paul, Pete, Ringo"
+
+//concat method
+let older = ["Ringo", "John"]
+let younger = ["Paul", "George"]
+console.log(older + younger) // this convers the arrays to strings, returns Ringo,JohnPaul,George
+console.log(older.concat(younger)) // retuns [ 'Ringo', 'John', 'Paul', 'George' ] which combines the arrays and creates one array
+
+let relatedThings = [["Windows", "MacOS"], ["New York", "Chicago"]] // this is a multidimensional array: the .length of this array is actually 2, not 4 because there are 2 elements (which are arrays) within the array. 
+console.log(relatedThings[2]) // returns undefined: if we wanted to access "new york", this looks like the 3rd element of the array, however, this variable only has 2 array elements
+console.log(relatedThings[1][0]) //returns "New York" 
+//          ^variable
+//                       [1] access the second element of the array ["New York", "Chicago"]
+//                           [0] access the 1st variable within the second element of the array "New York"
+
+        //more complex example
+        let rsvpGroups = [["Jane", "Mel"], "Jack", ["Rohan", "David", "Meg"]]; //this is a 2 dimensional array, which has multiple arrays and strings available inside of it. 
+        
+        // If we wanted to access the elements inside of this array, we'd want to use loops -
+
+        for (let i = 0; i < rsvpGroups.length; i++) {
+            let element = rsvpGroups[i]; //getting the index value of the element (the number place) we use this to extract the right element
+
+            if (Array.isArray(element)) { //because the array is a combo of other arrays and strings, we want to have the loop do something specific with only the arrays.
+                for (let j=0; j < element.length; j++) { //this nested loop is meant to go through the nested array
+                    console.log(element[j]) //if this is an array within an array, find it, and show me the array
+                }
+            } else {
+                console.log(element) //if the element is not an array within an array, show me the string
+            }
+        }
+        /* returns 
+        Jane
+        Mel
+        Jack
+        Rohan
+        David
+        Meg
+        */ 
+
+let grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]; 
+
+console.log(grid[1]) // returns [4,5,6]: this is allowing us to grab the second row of the grid variable
+//if we want to get a specific column, the easiest method would be to use a loop
+let secondCol = []; // creating a variable with an empty array
+for(let i = 0; i < grid.length; i++) {
+    secondCol.push(grid[i][1]) //grid[i] represents each row (what we are looping through), and 1 represents the second element in each row.
+}
+console.log(secondCol); 
+        // returns [ 2, 5, 8 ]
